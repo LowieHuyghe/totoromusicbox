3630fa8c2b7d823a9eae334b7c28622b0b7d748a
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2021-02-14 15:32:19 +0100

    Improvements

--- src/run/music.sh
+++ src/run/music.sh
@@ -5,9 +5,11 @@ cd "$( dirname "$0" )"
 # Set volume lower
 echo "Setting volume"
 amixer sset PCM 70% >/dev/null
-
-# Maintenance
-./maintenance.sh &
+echo "Checking volume"
+if ! amixer sget PCM | grep 70% >/dev/null; then
+  echo "Failed to set volume"
+  exit 1
+fi
 
 # Play song
 echo "Playing song"
