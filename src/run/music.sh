747b9556ac8b116594ea631c2984f152969d9ea4
Lowie Huyghe <iam@lowiehuyghe.com>
2021-02-25 14:05:16 +0100

    Use own random sort as mpg123 random does not really seen random.

--- src/run/music.sh
+++ src/run/music.sh
@@ -15,5 +15,7 @@ fi
 # Play song
 echo "Playing song"
 while true; do
-  mpg123 -Z ../../music/*.mp3
+  # Use own sort as mpg123 does not really seem random.
+  # Feed them 10 songs at a time. When done, just loop.
+  find ../../music -name "*.mp3" | sort --random-sort | xargs -d '\n' -n10 mpg123
 done
