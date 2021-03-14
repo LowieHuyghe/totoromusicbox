0dd141aef5cba352f50b16284af5aab6a96040d4
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2021-03-14 14:59:34 +0100

    Fixed location of music

--- src/music/run/music.sh
+++ src/music/run/music.sh
@@ -17,5 +17,5 @@ echo "Playing song"
 while true; do
   # Use own sort as mpg123 does not really seem random.
   # Feed them 10 songs at a time. When done, just loop.
-  find ../../music -name "*.mp3" | sort --random-sort | xargs -d '\n' -n10 mpg123
+  find ../../../music -name "*.mp3" | sort --random-sort | xargs -d '\n' -n10 mpg123
 done
