976bdd3b284627e3bb9961e6d82c88669cb89339
Lowie Huyghe <LowieHuyghe@users.noreply.github.com>
2021-03-14 14:56:28 +0100

    No cleanup

--- src/volume/run/volume.py
+++ src/volume/run/volume.py
@@ -6,12 +6,8 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 12 to be an input pin and set initial value to be pulled low (off)
 
-try:
-  while True: # Run forever
-    if GPIO.input(7) == GPIO.HIGH:
-      print("7 was pushed!")
-    if GPIO.input(11) == GPIO.HIGH:
-      print("11 was pushed!")
-
-finally:
-  GPIO.cleanup()
+while True: # Run forever
+  if GPIO.input(7) == GPIO.HIGH:
+    print("7 was pushed!")
+  if GPIO.input(11) == GPIO.HIGH:
+    print("11 was pushed!")
