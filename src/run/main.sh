0d686ae1cb90dcb8c62b89b36579d7455a0e7af7
Lowie Huyghe <iam@lowiehuyghe.com>
2021-03-17 17:36:27 +0100

    Run init instead of doing it through bash.

--- src/run/main.sh
+++ src/run/main.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
 cd "$( dirname "$0" )"
 
-# Make sure amixer is ready
-if amixer sset PCM "10%" >/dev/null && amixer sget PCM | grep "10%" >/dev/null; then
-  echo "amixer good to go"
-else
-  echo "amixer not ready yet"
-  exit 1
-fi
-
 # Volume
+./volume.py init
 ./volume.py &
 
 # Music
